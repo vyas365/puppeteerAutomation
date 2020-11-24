4891ac15f627a84c0c357f0bcad2401af5fc6c73
Vyas <Veda.V.Prabu@aexp.com>
2020-11-23 23:39:55 -0500

    cleanup selectors

--- src/selectors/pager.js
+++ src/selectors/pager.js
@@ -1,14 +1,23 @@
-// Example index=1 will be "First" -> usage pagerPositions(1)
-// index=2 will be "Previous" -> usage pagerPositions(2)
-// index=9 will be "Last"
-const pagerPosition = (index) => `[class='pagination']>li:nth-child(${index})`;
+// This function is used to store the selectors of the pages that are disabled
+// Example index = 1 will be "First" -> usage pagerPositionDisabled(1)
+// index = 2 will be "Previous" -> usage pagerPositionDisabled(2)
+// index = 9 will be "Last"
+
+const pagerPositionDisabled = (index) =>
+  `[class='pagination']>li[class='disabled']:nth-child(${index})`;
 
 const initialLoadPageDisabled = [
-  pagerPosition(1),
-  pagerPosition(2),
-  pagerPosition(3),
+  pagerPositionDisabled(1),
+  pagerPositionDisabled(2),
+  pagerPositionDisabled(3),
 ];
-const lastPageDisabled = [pagerPosition(7), pagerPosition(8), pagerPosition(9)];
+const lastPageDisabled = [
+  pagerPositionDisabled(7),
+  pagerPositionDisabled(8),
+  pagerPositionDisabled(9),
+];
+
+const pagerPosition = (index) => `[class='pagination']>li:nth-child(${index})`;
 
 module.exports = {
   first: pagerPosition(1),
